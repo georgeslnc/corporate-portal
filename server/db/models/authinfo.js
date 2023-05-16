541959984238d481f847520c7e69a7db8a061fb4
George <gslansky@yandex.ru>
2023-05-16 13:25:01 +0300

    auth-model

---
+++ server/db/models/authinfo.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class AuthInfo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  AuthInfo.init({
+    userId: DataTypes.INTEGER,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'AuthInfo',
+  });
+  return AuthInfo;
+};
