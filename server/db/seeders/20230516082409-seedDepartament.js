93dbb08e8406505c25f62c067805aa44ee184da0
George <gslansky@yandex.ru>
2023-05-16 12:22:37 +0300

    seed-generate

---
+++ server/db/seeders/20230516082409-seedDepartament.js
@@ -0,0 +1,46 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Departaments', [
+      {
+        title: 'Департамент продаж',
+        location: '1 этаж',
+        departamentHeadId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Финансовый департамент',
+        location: '2 этаж',
+        departamentHeadId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Департамент информационный технологий',
+        location: '3 этаж',
+        departamentHeadId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Админстративно-хозяйственный отдел',
+        location: '4 этаж',
+        departamentHeadId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Департамент по работе с персоналом',
+        location: '5 этаж',
+        departamentHeadId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Departament', null, {});
+  },
+};
