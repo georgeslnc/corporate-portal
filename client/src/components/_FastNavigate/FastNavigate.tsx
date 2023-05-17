71b36f3fcd191661dcc5a8ac68ef357e46237bd0
KIRILL EMANOV <124581178+KirillEmanov@users.noreply.github.com>
2023-05-18 01:08:39 +0800

    navbar 0.1

--- client/src/components/_FastNavigate/FastNavigate.tsx
+++ client/src/components/_FastNavigate/FastNavigate.tsx
@@ -1,68 +1,74 @@
-import { useNavigate } from 'react-router-dom';
-import './FastNavigate.css'
+import React, { useState } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import MailIcon from '@material-ui/icons/Mail';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Link, useLocation } from 'react-router-dom';
+import { AccountCircle, Chat, Description, Home, Inbox, MenuBook, PeopleAlt } from '@material-ui/icons';
+import useStyles from './navbarStyles';
 
-const FastNavigate = () => {
-  const navigate = useNavigate();
+const Navbar = () => {
+  const classes = useStyles();
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(location.pathname);
+
+  const handleLinkClick = (path) => {
+    setActiveLink(path);
+  };
+
+  const navLinks = [
+    { path: '/auth/login', name: 'Авторизация', icon: <AccountCircle /> },
+    { path: '/handbook', name: 'Справочник', icon: <MenuBook /> },
+    { path: '/tree', name: 'Структура компании', icon: <PeopleAlt /> },
+    { path: '/applications', name: 'Заявки', icon: <Inbox /> },
+    { path: '/newslist', name: 'Новости компании', icon: <MailIcon /> },
+    { path: '/', name: 'Главная', icon: <Home /> },
+    { path: '/chat', name: 'Чат', icon: <Chat /> },
+    { path: '/documents', name: 'Документы', icon: <Description /> },
+  ];
 
   return (
-    <div className='divStyle'>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/auth/login")}
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Корпоративный портал
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
       >
-        Login
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/handbook")}
-      >
-        Handbook
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/employee/:id")}
-      >
-        Employee
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/tree")}
-      >
-        Tree
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/applications")}
-      >
-        Applications
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/newslist")}
-      >
-        News List
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/")}
-      >
-        Home
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/chat")}
-      >
-        Chat
-      </button>
-      <button 
-        className='buttonStyle' 
-        onClick={() => navigate("/documents")}
-      >
-        Documents
-      </button>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List className={classes.listItemHover}>
+          {navLinks.map(({ path, name, icon }) => (
+            <ListItem
+              button
+              component={Link}
+              to={path}
+              key={name}
+              onClick={() => handleLinkClick(path)}
+              className={path === activeLink ? classes.activeLink : ''}
+            >
+              <ListItemIcon>{icon}</ListItemIcon>
+              <ListItemText primary={name} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 };
 
-
-export default FastNavigate;
+export default Navbar;
