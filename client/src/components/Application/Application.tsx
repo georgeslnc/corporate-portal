cd3f7008da80bbaed07fca11fffc716c5794cd57
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2023-05-18 19:23:35 +0300

    vfd

--- client/src/components/Application/Application.tsx
+++ client/src/components/Application/Application.tsx
@@ -1,20 +1,10 @@
-import React, { useState } from "react";
-import { RootState, useAppDispatch, useAppSelector } from "../../redux/type";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { postOffer } from "../../redux/Thunk/offer";
-import Bid from "./Bid";
-import {
-  Box,
-  Button,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-  TextField,
-  Typography,
-} from "@mui/material";
-import style from "./application.module.scss";
+import React, { useState } from 'react';
+import { RootState, useAppDispatch, useAppSelector } from '../../redux/type';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { postOffer } from '../../redux/Thunk/offer';
+import Bid from './Bid';
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material';
+import style from './application.module.scss';
 
 type Inputs = {
   value: string;
@@ -27,33 +17,27 @@ export type OfferData = {
 };
 
 export default function Application() {
-  const group = useAppSelector(
-    (state: RootState) => state.employeesSlice.group
-  );
-  const department = useAppSelector(
-    (state: RootState) => state.employeesSlice.department
-  );
+  const group = useAppSelector((state: RootState) => state.employeesSlice.group);
+  const department = useAppSelector((state: RootState) => state.employeesSlice.department);
   const dispatch = useAppDispatch();
-  const [selectedDepartment, setSelectedDepartment] = useState<string | null>(
-    "0"
-  );
-  const [titleDep, setTitleDep] = useState("");
-  const [titleGroup, settitleGroup] = useState("");
-  const [selectedValue, setSelectedValue] = useState<string>("1");
+  const [selectedDepartment, setSelectedDepartment] = useState<string | null>('0');
+  const [titleDep, setTitleDep] = useState('');
+  const [titleGroup, settitleGroup] = useState('');
+  const [selectedValue, setSelectedValue] = useState<string>('1');
   const { register, handleSubmit, reset } = useForm<Inputs>();
 
   const eventDepartment = (e: SelectChangeEvent) => {
     const selectedOption = e.target.value;
     const depId = department.find((el) => el.title === selectedOption)?.id;
-    setTitleDep(selectedOption || "");
-    settitleGroup("");
+    setTitleDep(selectedOption || '');
+    settitleGroup('');
     setSelectedDepartment(`${depId}` || null);
   };
 
   const selectGroup = (e: SelectChangeEvent) => {
     const selectedOption = e.target.value;
     const groupId = group.find((el) => el.title === selectedOption)?.id;
-    settitleGroup(selectedOption || "");
+    settitleGroup(selectedOption || '');
     setSelectedValue(`${groupId}`);
   };
 
@@ -64,7 +48,7 @@ export default function Application() {
       groupId: selectedValue,
       deadline: time,
     };
-    console.log("fsd");
+    console.log('fsd');
     reset();
     dispatch(postOffer(offerData));
   };
@@ -74,11 +58,9 @@ export default function Application() {
       <Typography variant="h5" component="h2">
         Выберите департамент и отдел для заполения заявки на отдел
       </Typography>
-      <Box sx={{ width: "400px", marginTop: "30px" }}>
+      <Box sx={{ width: '400px', marginTop: '30px' }}>
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">
-            Выбор департамента
-          </InputLabel>
+          <InputLabel id="demo-simple-select-label">Выбор департамента</InputLabel>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
@@ -93,7 +75,7 @@ export default function Application() {
             ))}
           </Select>
         </FormControl>
-        <FormControl fullWidth sx={{ marginTop: "30px" }}>
+        <FormControl fullWidth sx={{ marginTop: '30px' }}>
           <InputLabel id="demo-simple-select-label">Выбор отдела</InputLabel>
           <Select
             labelId="demo-simple-select-label"
@@ -107,42 +89,40 @@ export default function Application() {
                 <MenuItem key={element.id} value={element.title}>
                   {element.title}
                 </MenuItem>
-              ) : null
+              ) : null,
             )}
           </Select>
         </FormControl>
       </Box>
       {titleGroup && (
         <>
-          <Typography variant="h5" component="h2" sx={{ marginTop: "50px" }}>
-            Коротко укажите вашу проблему и желаемое время на устрание(минимум 1
-            час)
+          <Typography variant="h5" component="h2" sx={{ marginTop: '50px' }}>
+            Коротко укажите вашу проблему и желаемое время на устрание(минимум 1 час)
           </Typography>
-          <Typography variant="h6" component="h2" sx={{ fontSize: "14px" }}>
-            Если ваша заявка требует неотложна зайдите в справочник и свяжитесь
-            на прямую
+          <Typography variant="h6" component="h2" sx={{ fontSize: '14px' }}>
+            Если ваша заявка требует неотложна зайдите в справочник и свяжитесь на прямую
           </Typography>
           <Box
             sx={{
               width: 400,
-              display: "flex",
-              flexDirection: "column",
-              borderColor: "divider",
+              display: 'flex',
+              flexDirection: 'column',
+              borderColor: 'divider',
               gap: 1,
-              marginTop: "50px",
+              marginTop: '50px',
             }}
             component="form"
             onSubmit={handleSubmit(onSubmit)}
           >
-            <Typography variant="h4" component="h2" sx={{ fontSize: "14px" }}>
+            <Typography variant="h4" component="h2" sx={{ fontSize: '14px' }}>
               Текст заявки
             </Typography>
-            <TextField type="text" {...register("value")} />
-            <Typography variant="h4" component="h2" sx={{ fontSize: "14px" }}>
+            <TextField type="text" {...register('value')} />
+            <Typography variant="h4" component="h2" sx={{ fontSize: '14px' }}>
               Желаемое время на выполнение(считается в часах)
             </Typography>
-            <TextField type="number" {...register("time")} />
-            <Button type="submit" sx={{ background: "rgb(203, 210, 218)" }}>
+            <TextField type="number" {...register('time')} />
+            <Button type="submit" sx={{ background: 'rgb(203, 210, 218)' }}>
               Подать заявку
             </Button>
           </Box>
--- client/src/components/Application/Bid.tsx
+++ client/src/components/Application/Bid.tsx
@@ -1,27 +1,26 @@
-import React from "react";
-import { RootState ,useAppDispatch, useAppSelector } from "../../redux/type";
-import { changeStatusOffer } from "../../redux/Thunk/changeStatusOffer";
+import React from 'react';
+import { RootState, useAppDispatch, useAppSelector } from '../../redux/type';
+import { changeStatusOffer } from '../../redux/Thunk/changeStatusOffer';
 
 const userData: string | null = localStorage.getItem('userData');
 const parsedUserData: { groupId: number } = userData ? JSON.parse(userData) : null;
-const groupId: number = parsedUserData ? parsedUserData.groupId : 3 ;
+const groupId: number = parsedUserData ? parsedUserData.groupId : 3;
 
 export default function Bid() {
   const offer = useAppSelector((state: RootState) => state.employeesSlice.offer);
   const dispatch = useAppDispatch();
-  console.log(offer)
+  console.log(offer);
   return (
     <>
       <ul>
         {offer?.map((el) => {
           if (el.groupId === groupId && !el.status) {
             return (
-
-            <li key={el.id}>
-              <h4>{el.title}</h4>
-              <button onClick={() => dispatch(changeStatusOffer(el.id))}>надо сделать</button>
-            </li>
-            )
+              <li key={el.id}>
+                <h4>{el.title}</h4>
+                <button onClick={() => dispatch(changeStatusOffer(el.id))}>надо сделать</button>
+              </li>
+            );
           }
           return null;
         })}
