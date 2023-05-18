a0c073ad427a8fd7ea23c4a868b1c5c54a8c598d
George Slanskiy <124250538+georgeslnc@users.noreply.github.com>
2023-05-18 15:32:47 +0300

    style-tree

--- client/src/components/Tree/AllGroup.tsx
+++ client/src/components/Tree/AllGroup.tsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Group, RootState, useAppSelector } from '../../redux/type';
-import { Link } from 'react-router-dom';
+import { List, ListItem, ListItemText, Divider } from '@mui/material';
 
 type DepartamentId = {
   departamentId: number;
 };
 
 export default function AllGroup(departamentId: DepartamentId) {
-
   const groups = useAppSelector((state: RootState) => state.employeesSlice.group);
 
-  const selectedGroup = groups.filter((group:Group) => group.departamentId === departamentId.departamentId)
+  const navigate = useNavigate()
+
+  const handleClick = (id:number) => (
+    navigate(`/tree/${id}`)
+  )
+
+  const selectedGroup = groups.filter(
+    (group: Group) => group.departamentId === departamentId.departamentId
+  );
+
 
   return (
-    <ul>
-    {selectedGroup.map((group: Group) => (
-      <li key={group.id}>
-        <Link to={`/tree/${group.id}`}>{group.title}</Link>
-      </li>
-    ))}
-  </ul>
-  )
+    <List component="nav" aria-label="mailbox folders">
+      {selectedGroup.map((group: Group) => (
+        <ListItem
+          key={group.id}
+          component="div"
+          onClick={() => handleClick(group.id)}
+          sx={{
+            marginLeft: '15px',
+            textDecoration: 'none',
+            cursor: 'pointer',
+            '&:hover': {
+              textDecoration: 'underline',
+            },
+          }}
+        >
+          {group.title}
+        </ListItem>
+      ))}
+    </List>
+  );
 }
+
+
