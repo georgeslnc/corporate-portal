4bb0d300a2588255103d3b0661fd50dfb6fa895c
George Slanskiy <124250538+georgeslnc@users.noreply.github.com>
2023-05-20 14:02:58 +0300

    file-title-done

--- client/src/components/Documents/Documents.tsx
+++ client/src/components/Documents/Documents.tsx
@@ -7,7 +7,7 @@ import { getFiles } from '../../redux/Thunk/files/getFiles';
 import { File } from '../../redux/type';
 import DocumentsAudit from './DocumentsAudit';
 import DocumentsHR from './DocumentsHR';
-import { Button, FormControl, InputLabel, MenuItem, Select, Typography, Input } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select, Typography, Input, Grid } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 
 export default function Documents() {
@@ -36,27 +36,41 @@ export default function Documents() {
     <div>
       {professionId === 5 ? (
         <>
-          <Typography variant="h6">Добавление документов</Typography>
           <form onSubmit={uploadFileHandler}>
-            <div>
-              <FormControl>
-                <Input id="file" type="file" name="file" disableUnderline />
-              </FormControl>
-              <FormControl>
-                <Input id="file" type="text" name="adminTitle" placeholder="Название файла для отображения" disableUnderline />
-              </FormControl>
-            </div>
-            <div>
-              <FormControl>
-                <Select labelId="category-label" name="documentType" defaultValue="Документы по работе с персоналом">
-                  <MenuItem value="Документы по работе с персоналом">Документы по работе с персоналом</MenuItem>
-                  <MenuItem value="Документу бухгалтерии">Документу бухгалтерии</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-            <Button type="submit" variant="contained" size="small" endIcon={<SendIcon />}>
-              Загрузить документ
-            </Button>
+            <Grid container direction="column" spacing={2}>
+              <Grid item>
+                <Typography variant="h6">Добавление документов</Typography>
+              </Grid>
+              <Grid item>
+                <FormControl>
+                  <Input id="file" type="file" name="file" disableUnderline />
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <FormControl>
+                  <Input
+                    id="adminTitle"
+                    type="text"
+                    name="adminTitle"
+                    placeholder="Название файла для отображения"
+                    disableUnderline
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <FormControl>
+                  <Select labelId="category-label" name="documentType" defaultValue="Документы по работе с персоналом">
+                    <MenuItem value="Документы по работе с персоналом">Документы по работе с персоналом</MenuItem>
+                    <MenuItem value="Документу бухгалтерии">Документу бухгалтерии</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <Button type="submit" variant="contained" size="small" endIcon={<SendIcon />}>
+                  Загрузить документ
+                </Button>
+              </Grid>
+            </Grid>
           </form>
           <DocumentsHR filesHr={filesHr} />
           <DocumentsAudit filesAudit={filesAudit} />
