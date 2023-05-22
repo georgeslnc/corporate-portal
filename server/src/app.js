b77627d70d7bc00e5497b8237ca907ef3901fb58
Михаил Солонский <msolonsky@gmail.com>
2023-05-22 18:42:36 +0200

    server:add uploader employee photo, client: add from photo

--- server/src/app.js
+++ server/src/app.js
@@ -115,6 +115,7 @@ app.use('/login', (req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, '../public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(dbCheck);
--- server/src/routes/admin/addEmployees.route.js
+++ server/src/routes/admin/addEmployees.route.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const multer = require('multer');
-const fs = require('fs');
+const path = require('path');
 const { Employee, Group, Profession } = require('../../../db/models');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'public/user-photo');
+    cb(null, 'public/');
   },
   filename(req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
+    const extension = file && path.extname(file.originalname);
+    cb(null, `photo-user-${Date.now()}${extension}`);
   },
 });
 
@@ -22,54 +23,48 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage, fileFilter });
 
-/*
-
-#
-#
-#
-#
-#
-*/
-
 const phoneFormatter = require('../../utils/phoneFormatter');
 const validateModel = require('../../utils/validateModel');
 
-router.post('/employees', upload.single('photo'), async (req, res) => {
-  try {
-    const {
-      firstName, middleName, lastName, groupTitle, profession, email, phoneNumber, birthday
-    } = req.body;
+router
+  .post('/employees', upload.single('photo'), async (req, res) => {
+    try {
+      const {
+        firstName, middleName, lastName, groupTitle, profession, email, phoneNumber, birthday
+      } = req.body;
 
-    const phone = phoneFormatter(phoneNumber);
-    // const photoUrl = 'https://i.pravatar.cc/300'; //*  удалить заглушку
-    const photoUrl = req.file.path;
+      const phone = phoneFormatter(phoneNumber);
+      const photoUrl = req.file && process.env.BASE_URL + req.file.path.replace('public/', '');
 
-    const groupId = await validateModel(Group, { title: groupTitle });
-    const professionId = await validateModel(Profession, { position: profession });
-    if (!groupId || !professionId) {
-      return res.status(400).send({ message: 'Отдел или профессия не найдены' });
+      console.log('|______|  photoUrl:', photoUrl);
+
+      const groupId = await validateModel(Group, { title: groupTitle });
+      const professionId = await validateModel(Profession, { position: profession });
+
+      if (!groupId || !professionId) {
+        return res.json({ status: 409, message: 'Отдел или профессия не найдены' });
+      }
+
+      const checkData = Object.values(req.body).every(Boolean);
+      if (!checkData) {
+        return res.json({ status: 400, message: 'Не все поля заполнены' });
+      }
+
+      const checkUser = await Employee.findOne({ where: { email } }, { raw: true });
+
+      if (checkUser) {
+        return res.json({ status: 400, message: 'Сотрудник с такими данными уже существует' });
+      }
+
+      const user = await Employee.create({
+        firstName, middleName, lastName, groupId, professionId, email, phone, birthday, photoUrl
+      });
+
+      return res.json({ status: 200, message: 'Сотрудник успешно добавлен' });
+    } catch (error) {
+      console.error('===> error', error);
+      return res.json({ status: 500, message: 'Ошибка сервера.' });
     }
-
-    const checkData = Object.values(req.body).every(Boolean);
-    if (!checkData) {
-      return res.status(400).send({ message: 'Не все поля заполнены' });
-    }
-
-    const checkUser = await Employee.findOne({ where: { email } }, { raw: true });
-
-    if (checkUser) {
-      return res.status(400).send({ message: 'Сотрудник с такими данными уже существует' });
-    }
-
-    const user = await Employee.create({
-      firstName, middleName, lastName, groupId, professionId, email, phone, birthday, photoUrl
-    });
-
-    return res.status(200).send({ message: 'Сотрудник успешно создан' });
-  } catch (error) {
-    console.error('===> error', error);
-    return res.status(500).send({ message: 'Ошибка сервера.' });
-  }
-});
+  });
 
 module.exports = router;
